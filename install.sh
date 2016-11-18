076acd5345afe7617398e60c200db783a6f2f0cb
Martin Schneider <martinfs@mit.edu>
2016-11-18 16:17:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Install and run this easily using
+# wget -O - http://goo.gl/SerBjI | bash
+
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
