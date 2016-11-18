c90887872d37d1592d52a8eaa7885f7166d43830
Martin Schneider <martinfs@mit.edu>
2016-11-18 16:13:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 sudo apt-get install libopencv-dev python-opencv
-git clone https://github.com/mfranzs/cv_class.git
+git clone https://github.com/mfranzs/splash_cv_class.git
 cd cv_class
 subl read_webcam.py
